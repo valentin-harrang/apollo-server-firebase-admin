aa6e285e437b9489c24ea0f12c18d7bca651fdd0
valentin <valentin.harrang@silae.fr>
2023-03-31 19:56:37 +0200

    Utilisation de serverless

---
+++ src/server.ts
@@ -0,0 +1,90 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+import admin from 'firebase-admin';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT_KEY || '')),
+});
+
+const typeDefs = `#graphql
+  type User {
+    uid: ID!
+    email: String
+    phoneNumber: String
+    displayName: String
+    photoURL: String
+  }
+
+  input CreateUserInput {
+    email: String
+    phoneNumber: String
+    password: String!
+    displayName: String
+    photoURL: String
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
+  }
+
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Mutation: {
+    async createUser(_, { input }) {
+      try {
+        const { email, phoneNumber, password, displayName, photoURL } = input;
+
+        let userRecord;
+
+        // Check if the user wants to use email or phone number
+        if (email) {
+          // Create user in Firebase Authentication with email
+          userRecord = await admin.auth().createUser({
+            email,
+            password,
+            displayName,
+            photoURL,
+          });
+        } else if (phoneNumber) {
+          // Create user in Firebase Authentication with phone number
+          userRecord = await admin.auth().createUser({
+            phoneNumber,
+            password,
+            displayName,
+            photoURL,
+          });
+        } else {
+          throw new Error(
+            "You must provide either an email or a phone number."
+          );
+        }
+
+        // Return user information
+        return {
+          uid: userRecord.uid,
+          email: userRecord.email,
+          phoneNumber: userRecord.phoneNumber,
+          displayName: userRecord.displayName,
+          photoURL: userRecord.photoURL,
+        };
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+);
